3a4d24560e014a5500acbcd0661d703a98e8ef28
Logan Harris <loganrodharris@gmail.com>
2020-11-09 21:15:38 +1100

    added loop to repeatedly beep and boop

--- src/main.py
+++ src/main.py
@@ -3,13 +3,15 @@ import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-string = input("Input what you wish to beep/boop:\n")
+while True:
+    print("Type 'exit' or press Ctrl+C to quit")
+    string = input("Input what you wish to beep/boop:\n")
 
-for byte in bytearray(string, "utf8"):
-    binary_str = bin(byte)
-    binary_str = binary_str[2::]
-    for binary in binary_str:
-        if binary == "1":
-            playsound("sounds/beep.mp3")
-        else:
-            playsound("sounds/boop.mp3")  
+    for byte in bytearray(string, "utf8"):
+        binary_str = bin(byte)
+        binary_str = binary_str[2::]
+        for binary in binary_str:
+            if binary == "1":
+                playsound("sounds/beep.mp3")
+            else:
+                playsound("sounds/boop.mp3")  
