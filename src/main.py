e8580615af0c134b45c63d009d4df45dff13d2c9
Logan Harris <loganrodharris@gmail.com>
2020-11-09 21:27:42 +1100

    added some error handling

--- src/main.py
+++ src/main.py
@@ -3,15 +3,19 @@ import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-while True:
-    print("Type 'exit' or press Ctrl+C to quit")
-    string = input("Input what you wish to beep/boop:\n")
+try:
+    while True:
+        print("Type 'exit' or press Ctrl+C to quit")
+        string = input("Input what you wish to beep/boop:\n")
 
-    for byte in bytearray(string, "utf8"):
-        binary_str = bin(byte)
-        binary_str = binary_str[2::]
-        for binary in binary_str:
-            if binary == "1":
-                playsound("sounds/beep.mp3")
-            else:
-                playsound("sounds/boop.mp3")  
+        for byte in bytearray(string, "utf8"):
+            binary_str = bin(byte)
+            binary_str = binary_str[2::]
+            for binary in binary_str:
+                if binary == "1":
+                    playsound("sounds/beep.mp3")
+                else:
+                    playsound("sounds/boop.mp3")  
+except:
+    raise Exception
+    input("")
